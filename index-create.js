cdd53409224291d4c887eb582b72044a3e0fbe91
Thiago Pacheco <thiagodelimapacheco@gmail.com>
2021-05-11 22:34:06 -0400

    Created minimal cli

--- index-create.js
+++ index-create.js
@@ -2,28 +2,44 @@ const fs = require("fs");
 const program = require("commander");
 const { createFolder } = require("./utils");
 const { series } = require("async");
-const { exec } = require("child_process");
-const loading = require("loading-cli");
+const util = require("util");
+const exec = util.promisify(require("child_process").exec);
+const ora = require("ora");
+
+const Listr = require("listr");
+
+let spinner;
 
 async function createApp() {
   program.parse(process.argv);
-
   const appName = program.args[0];
+  const tasks = new Listr([
+    {
+      title: "Create app folder",
+      task: () => createFolder(appName),
+    },
+    {
+      title: "Copy project files",
+      task: () => copyFiles(appName),
+    },
+    {
+      title: "Initialize git",
+      task: () => initializeGit(),
+    },
+    {
+      title: "Install dependencies",
+      task: () => installPackages(appName),
+    },
+  ]);
+  await tasks.run();
 
-  createFolder(appName);
-
-  await copyFiles(appName);
-
-  await installPackages(appName);
+  await appInstructions(appName);
 }
 
 async function copyFiles(appName) {
-  const load = loading("Copying files").start();
   const templatePath = `${__dirname}/files`;
   const currentPath = process.cwd();
-
   const files = fs.readdirSync(templatePath);
-
   files.forEach((file) => {
     const origFilePath = `${templatePath}/${file}`;
     const writePath = `${currentPath}/${appName}/${file}`;
@@ -32,12 +48,20 @@ async function copyFiles(appName) {
       .replace("<APPNAME>", appName);
     fs.writeFileSync(writePath, contents, "utf8");
   });
-  load.stop();
 }
 
 async function installPackages(appName) {
-  exec(`npm install --prefix ${appName} @pachecoio/logan`);
-  exec(`npm install --prefix ${appName} --save-dev`);
+  await exec(`npm install --prefix ${appName} @pachecoio/logan`);
+  await exec(`npm install --prefix ${appName} --save-dev`);
+}
+
+async function initializeGit() {}
+
+async function appInstructions(appName) {
+  console.log(`\n\nLogan app created successfully`);
+  console.log("Next steps\n");
+  console.log(`    cd ${appName}`);
+  console.log(`    npm run dev:start\n\n`);
 }
 
 createApp();
--- index.js
+++ index.js
@@ -1,20 +1,38 @@
 const chalk = require("chalk");
 const clear = require("clear");
 const figlet = require("figlet");
-const program = require("commander");
+const loading = require("loading-cli");
 
-clear();
-console.log(
-  chalk.red(figlet.textSync("logan-cli", { horizontalLayout: "full" }))
-);
+async function main() {
+  clear();
+  console.log(
+    chalk.red(figlet.textSync("logan-cli", { horizontalLayout: "full" }))
+  );
 
-program
-  .version("0.0.1")
-  .description("A simple CLI to generate a Logan app")
-  .command("create [name]", "Create a new logan app")
-  .option("-p, --peppers", "Add peppers")
-  .option("-P, --pineapple", "Add pineapple")
-  .option("-b, --bbq", "Add bbq sauce")
-  .option("-c, --cheese <type>", "Add the specified type of cheese [marble]")
-  .option("-C, --no-cheese", "You do not want any cheese")
-  .parse(process.argv);
+  try {
+    let load = loading("Starting setup");
+    const program = await initProgram();
+    load.stop();
+  } catch (error) {
+    console.log("Not able to proceed with the command \n", error);
+  }
+}
+
+async function initProgram() {
+  const program = require("commander");
+
+  program
+    .version("0.0.1")
+    .description("A simple CLI to generate a Logan app")
+    .command("create [name]", "Create a new logan app")
+    .option("-p, --peppers", "Add peppers")
+    .option("-P, --pineapple", "Add pineapple")
+    .option("-b, --bbq", "Add bbq sauce")
+    .option("-c, --cheese <type>", "Add the specified type of cheese [marble]")
+    .option("-C, --no-cheese", "You do not want any cheese")
+    .parse(process.argv);
+
+  return program;
+}
+
+main();
