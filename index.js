cf1798a9b93abc567b426f182831641001236878
Thiago Pacheco <thiagodelimapacheco@gmail.com>
2021-06-03 22:42:13 -0400

    Removed unnecessary options

--- index.js
+++ index.js
@@ -23,11 +23,6 @@ async function initProgram() {
     .version("0.0.1")
     .description("A simple CLI to generate a Logan app")
     .command("create [name]", "Create a new logan app")
-    .option("-p, --peppers", "Add peppers")
-    .option("-P, --pineapple", "Add pineapple")
-    .option("-b, --bbq", "Add bbq sauce")
-    .option("-c, --cheese <type>", "Add the specified type of cheese [marble]")
-    .option("-C, --no-cheese", "You do not want any cheese")
     .parse(process.argv);
 
   return program;
